f66cb13a694bbeab5fc7d8b41e883d3639406d50
surendrakumarperumalla <perumallasurendra12345@gmail.com>
2024-04-17 16:54:15 +0530

    change

---
+++ project/items.js
@@ -0,0 +1,151 @@
+var items={
+    starters:[
+        {
+            itemname:"chilli chicken",
+            itemprice:900,
+            quantity:1,
+            quantityunits:"kg"
+        },
+        {
+            itemname:"panner",
+            itemprice:1200,
+            quantity:1,
+            quantityunits:"kg"
+        }, 
+        
+        {
+            itemname:"chicken lollipop",
+            itemprice:800,
+            quantity:1,
+            quantityunits:"kg"
+        }, 
+        
+        {
+            itemname:"vegmanchuria",
+            itemprice:500,
+            quantity:1,
+            quantityunits:"kg"
+        },
+    ],
+    liquids:[ 
+        {
+            itemname:"sambar",
+            itemprice:600,
+            quantity:1,
+            quantityunits:"litre"
+        },
+        
+        
+        {
+            itemname:"curd-chutney",
+            itemprice:400,
+            quantity:1,
+            quantityunits:"litre"
+        }, 
+        
+        {
+            itemname:"rasam",
+            itemprice:300,
+            quantity:1,
+            quantityunits:"litre"
+        },  
+        
+        {
+            itemname:"chicken-soup",
+            itemprice:900,
+            quantity:1,
+            quantityunits:"litre"
+        },
+    ],
+    curries:[ 
+        {
+            itemname:"chicken",
+            itemprice:1200,
+            quantity:1,
+            quantityunits:"kg"
+        },
+
+        {
+            itemname:"panner curry",
+            itemprice:1500,
+            quantity:1,
+            quantityunits:"kg"
+        }, 
+        
+        {
+            itemname:"mutton",
+            itemprice:2000,
+            quantity:1,
+            quantityunits:"kg"
+        }, 
+        
+        {
+            itemname:"fish",
+            itemprice:800,
+            quantity:1,
+            quantityunits:"kg"
+        },
+        
+    ],
+    desserts:[ 
+        {
+            itemname:"ice cream",
+            itemprice:900,
+            quantity:1,
+            quantityunits:"kg"
+        },
+
+        {
+            itemname:"cake",
+            itemprice:1200,
+            quantity:1,
+            quantityunits:"kg"
+        }, 
+        
+        {
+            itemname:"gulab jamun",
+            itemprice:1100,
+            quantity:1,
+            quantityunits:"kg"
+        }, 
+        
+        {
+            itemname:"sweets",
+            itemprice:700,
+            quantity:1,
+            quantityunits:"kg"
+        },
+    ],
+    rice:[
+        {
+            itemname:"biryani",
+            itemprice:1000,
+            quantity:1,
+            quantityunits:"kg"
+        },
+
+        {
+            itemname:"white rice",
+            itemprice:800,
+            quantity:1,
+            quantityunits:"kg"
+        }, 
+        
+        {
+            itemname:"pulihora",
+            itemprice:900,
+            quantity:1,
+            quantityunits:"kg"
+        },
+        
+        {
+            itemname:"green rice",
+            itemprice:600,
+            quantity:1,
+            quantityunits:"kg"
+        },
+    ],
+    
+}
+
+
